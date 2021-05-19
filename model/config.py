333d75a2a3f310ab397924d4832459a412ddc7e4
Matt Barlin <33430774+matttyb80@users.noreply.github.com>
2021-05-19 15:30:14 -0400

    config and sim setup my way

--- model/config.py
+++ model/config.py
@@ -1,19 +1,42 @@
-from cadCAD import configuration
+from cadCAD.configuration import Experiment
+from cadCAD.configuration.utils import config_sim
 
 from .psub import psubs
 from .state import genesis_state
 from .sys_params import params
+from .sim_setup import SIMULATION_TIME_STEPS, MONTE_CARLO_RUNS
 
 # Parameters
 # Values are lists because they support sweeping.
-simulation_config = configuration.utils.config_sim({
-    "T": range(100),
-    "N": 1,
-    'M': params
+simulation_config = config_sim({
+        'N': MONTE_CARLO_RUNS, 
+        'T': range(SIMULATION_TIME_STEPS), # number of timesteps
+        'M': params,
 })
 
-exp = configuration.Experiment()
+exp = Experiment()
 
 exp.append_configs(sim_configs=simulation_config,
                    initial_state=genesis_state,
                    partial_state_update_blocks=psubs)
+
+
+# from cadCAD import configuration
+
+# from .psub import psubs
+# from .state import genesis_state
+# from .sys_params import params
+
+# # Parameters
+# # Values are lists because they support sweeping.
+# simulation_config = configuration.utils.config_sim({
+#     "T": range(100),
+#     "N": 1,
+#     'M': params
+# })
+
+# exp = configuration.Experiment()
+
+# exp.append_configs(sim_configs=simulation_config,
+#                    initial_state=genesis_state,
+#                    partial_state_update_blocks=psubs)
