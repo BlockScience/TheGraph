340ca2aa794f4e7d3571218cae8fc2fc1670a5ef
Joshua Stafford <jstaff@gmail.com>
2021-07-26 13:11:37 -0400

    Negative shares issue resolved.

--- model/state.py
+++ model/state.py
@@ -1,6 +1,8 @@
 # from model.parts import delegator
 from .parts.delegator import Delegator
 
+initial_shares = 10
+
 """ System state/state of the delegation pool for one indexer. """
 genesis_state = {
     # NOTE: make these a parameter
@@ -12,15 +14,13 @@ genesis_state = {
     'pool_locked_stake': 0,  # amount of GRT locked in undelegation process.  these are NO LONGER in the delegated pool and also not owned by delegator yet.
     
     # S
-    "shares": 0,  # shares--this is only added to when a delegator delegates
+    "shares": initial_shares,  # shares--this is only added to when a delegator delegates
     
     # id=0 is the original provider of 10 pool_delegated_stake and owns 10 shares    
     # TODO: use minimum_shares=params['s_del']
-    "delegators": {0: Delegator(shares=10, minimum_shares=10)},
+    "delegators": {0: Delegator(shares=initial_shares, minimum_shares=10)},
     "period_revenue": 0,  # this is passed directly to the delegators
-    "spot_price": 2,
     'GRT': 10000000,
-
     'indexing_revenue': 0,
     'query_revenue': 0,
     'indexer_revenue': 0,
