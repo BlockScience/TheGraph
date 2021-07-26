6989c952948ac1eb6208e5ca001cb529ce658339
Joshua Stafford <jstaff@gmail.com>
2021-07-26 12:52:54 -0400

    Model runs!

--- model/parts/delegator_behaviors_bookkeeping.py
+++ model/parts/delegator_behaviors_bookkeeping.py
@@ -87,13 +87,13 @@
 def store_shares(params, step, sL, s, inputs):
     key = 'shares'
     # add shares of all delegators
-    shares = sum([d.shares for d in s['delegators']])    
+    shares = sum([d.shares for d in s['delegators'].values()])    
     return key, shares
 
 
 def store_pool_delegated_stake(params, step, sL, s, inputs):
     key = 'pool_delegated_stake'
-    pool_delegated_stake = sum([d.delegated_tokens for d in s['delegators']])    
+    pool_delegated_stake = sum([d.delegated_tokens for d in s['delegators'].values()])    
     return key, pool_delegated_stake
 
 def increment_epoch(params, step, sL, s, inputs):
