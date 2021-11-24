ac703335b27859a9a16733093517d51fcb9564fd
jesse <jesse.p.tao@gmail.com>
2021-11-24 14:11:33 -0700

    add delegator object

---
+++ model/parts/portfolio.py
@@ -0,0 +1,16 @@
+from decimal import Decimal
+
+class Portfolio():
+    def __init__(self, id, holdings = Decimal(100000000), ROI = Decimal(0), indexer_shares = {},
+                indexer_revenues = {}, indexer_price = {}, indexer_realized_price = {}, indexer_yield = {},
+                indexer_in_tokens = {}):
+        self.id = id
+        self.holdings = holdings
+        self.ROI = ROI
+        self.indexer_shares = indexer_shares
+        self.indexer_revenues = indexer_revenues
+        self.indexer_price = indexer_price
+        self.indexer_realized_price = indexer_realized_price
+        self.indexer_yield = indexer_yield
+        self.indexer_in_tokens = indexer_in_tokens
+        
