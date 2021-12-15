fddfdaaab8cfbcc850a78636ec8044a127031994
jesse <jesse.p.tao@gmail.com>
2021-12-15 16:37:56 -0700

    add simple analysis of single delegator portfolio

--- model/sim_setup.py
+++ model/sim_setup.py
@@ -4,7 +4,7 @@ print("running file: sim_setup.py")
 # SIMULATION_TIME_STEPS = 1576
 
 # 3 indexers
-SIMULATION_TIME_STEPS = 1530
+SIMULATION_TIME_STEPS = 1520
 
 # all indexers
 # SIMULATION_TIME_STEPS = 90957
