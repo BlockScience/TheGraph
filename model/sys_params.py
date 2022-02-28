d8776573abdb16fab99b2510f60f629f6a95f3db
matttyb80 <mbarlin@gmail.com>
2022-02-28 17:17:07 -0500

    subtract shares from portfoli

--- model/sys_params.py
+++ model/sys_params.py
@@ -18,8 +18,8 @@ GRT_conversion_rate = -18
 
 # TODO: check this tax rate out--0% passes tests, 0.005 does not.
 # delegation tax rate is 0.5% as documented here: https://thegraph.com/docs/delegating#delegation-risks
-# delegation_tax_rate = [Decimal(0)]
-delegation_tax_rate = [Decimal(0.005)]
+delegation_tax_rate = [Decimal(0)]
+# delegation_tax_rate = [Decimal(0.005)]
 delegation_leverage = [16]
 
 delegator_initial_holdings = [Decimal(10e9)]
